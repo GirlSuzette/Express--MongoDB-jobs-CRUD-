4ed3757647a3662ee3ddedc3473d2d80f9285bbc
GirlSuzette <trinitysuzette_marpis@hotmail.com>
2019-01-28 18:24:13 -0600

    MongoDB-Jobs

---
+++ src/routes/api.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const app = Router();
+
+const Jobs = require('../controllers/jobs/jobs')
+
+
+
+//Jobs routes
+app.get('/jobs', Jobs.index);
+app.post('/jobs', Jobs.create);
+app.get('/jobs/:jobId', Jobs.findBy);
+app.delete('/jobs/:jobId', Jobs.removeBy);
+app.put('/jobs/:jobId', Jobs.update);
+
+
+
+module.exports = app;
